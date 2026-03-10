d8ab452bb7bd1a86d2a366c5374589731e4e99ee
idlakshan <id.lakshan21@gmail.com>
2026-03-10 16:19:57 +0530

    feat: add navbar

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,30 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { ClerkProvider } from '@clerk/clerk-react'
+import SignInPage from './pages/SignInPage.tsx'
+import SignUpPage from './pages/SignUpPage.tsx'
+
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+
+if (!PUBLISHABLE_KEY) {
+  throw new Error("Missing Publishable Key")
+}
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/">
+    
+        <BrowserRouter>
+          <Routes>
+            <Route path="/*" element={<App />} />
+            <Route path="/sign-in" element={<SignInPage />} />
+            <Route path="/sign-up" element={<SignUpPage />} />
+          </Routes>
+        </BrowserRouter>
+
+    </ClerkProvider>
+
+  </StrictMode>
 )
